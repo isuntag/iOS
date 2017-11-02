3764a97922005c35c9358cf4c47ee80d228dddfa
Isaac <isuntag@gmail.com>
2017-11-02 18:23:17 -0400

    Initial commit

---
+++ fundamentals/fundamentals2.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+// create a loop (either for or while) that prints all of the values from 1-255
+for i in 1...255 {
+    print(i)
+}
+
+// create a program that prints all of the values from 1-100 that are divisible by 3 or 5 but not both
+for i in 1...100 {
+    if((i % 3 == 0 || i % 5 == 0) && i % 15 != 0) {
+        print(i)
+    }
+}
+// modify that program to print "Fizz" when the number is divisible by 3 and "Buzz" when the number is divisible by 5 as well as "FizzBuzz" when the number is divisible by both!
+for i in 1...100 {
+    if(i % 3 == 0 && i % 5 == 0) {
+        print("FizzBuzz")
+    } else if(i % 3 == 0) {
+        print("Fizz")
+    } else if(i % 5 == 0) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
---
+++ fundamentals/stringconcat.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str += " you are so cool."
+// loop from 1 to 5 including 5
+for i in 1...5 {
+    print(i)
+}
+// loop from 1 to 5 excluding 5
+for i in 1..<5 {
+    print(i)
+}
